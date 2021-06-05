3553f9b0ba05a5c2097af041727f044f89d279aa
Ashish Kumar <sayhitoak@gmail.com>
2021-06-06 02:45:07 +0530

    Moving typedefs and resolvers to different folder

--- index.js
+++ index.js
@@ -2,38 +2,14 @@
 // resolvers and typedef
 // Connect apollo server with mongoDB
 
-const {ApolloServer,gql}  = require('apollo-server');
+const {ApolloServer}  = require('apollo-server');
 const mongoose = require('mongoose');
 
 const {MONGODB} = require('./config.js');
-const Post = require('./models/Post');
 
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
 
-
-const typeDefs = gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-    type Query {
-        getPosts : [Post]
-    }
-`;
-
-const resolvers = {
-    Query :{
-        async getPosts() {
-            try {
-              const posts = await Post.find();
-              return posts;
-            } catch (err) {
-              throw new Error(err);
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
